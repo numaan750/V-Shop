232ce00cb46653283589f289b7612edfbaf2fd50
Numaan Ali <alinumaan35@gmail.com>
2025-11-05 10:26:37 +0500

    qqq

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 // next.config.mjs
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-// simulate __dirname for ESM modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: __dirname, 
-  distDir: 'build',                
+  outputFileTracingRoot: __dirname,
+  // distDir: '.next', // default, can remove this line
 };
 
 export default nextConfig;
+
