1d4adda3b059027227e9904bb065ec46048e17c3
Numaan Ali <alinumaan35@gmail.com>
2025-11-05 10:27:50 +0500

    11

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 // next.config.mjs
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// simulate __dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingRoot: __dirname,
-  // distDir: '.next', // default, can remove this line
+  // distDir: 'build', // optional, use default .next
 };
 
 export default nextConfig;
-
